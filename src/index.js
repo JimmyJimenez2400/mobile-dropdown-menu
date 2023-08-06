2f30d54f97df87d2c0a1377284fe14df1deabc5b
Jimmy Jimenez <jimmy.jimenez@jjay.cuny.edu>
2023-08-06 00:09:04 -0400

    calling pageLoad function

--- src/index.js
+++ src/index.js
@@ -1,20 +1,4 @@
-import _ from "lodash";
 import "./style.css";
+import pageLoad from "./pageLoad/pageLoad";
 
-function hello() {
-  const sectionContainer = document.createElement("div");
-
-  const h1 = document.createElement("h1");
-  h1.innerHTML = _.join(["Hello", "webpack"], " ");
-  h1.classList.add("hello");
-
-  const h2 = document.createElement("h2");
-  h2.textContent = "HELLLLO WORLD";
-
-  sectionContainer.appendChild(h1);
-  sectionContainer.appendChild(h2);
-
-  return sectionContainer;
-}
-
-document.body.appendChild(hello());
+pageLoad();
