90c1018129095c72d3077007d00ba30ed4fc3435
Jimmy Jimenez <jimmy.jimenez@jjay.cuny.edu>
2023-08-06 00:10:03 -0400

    add event listeners

---
+++ src/mobile-dropdown-menu/mobileDropDownListener.js
@@ -0,0 +1,16 @@
+export default function mobileDropDownListener(e) {
+  const dropDownMenu = document.getElementById("menuOptions");
+
+  const toggleDropDown = function () {
+    dropDownMenu.classList.toggle("show");
+  };
+
+  e.stopPropagation();
+  toggleDropDown();
+
+  document.documentElement.addEventListener("click", () => {
+    if (dropDownMenu.classList.contains("show")) {
+      toggleDropDown();
+    }
+  });
+}
